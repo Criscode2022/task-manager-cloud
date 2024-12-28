2f03ed0df03ab0190115184e373462862dc1653a
Cristian Damil <cristiancode2023@gmail.com>
2024-12-28 15:56:33 +0100

    Add install button

--- src/app/core/types/alert-messages.ts
+++ src/app/core/types/alert-messages.ts
@@ -4,4 +4,5 @@ export enum AlertMessages {
   DeleteUserAlert = 'Are you sure that you want to delete your User ID? This will delete all your tasks from the cloud but will keep them on the device',
   DownloadAlert = 'Important: This will OVERWRITE your current offline tasks',
   InfoAlert = 'Upload the tasks to get an user ID, which you can use to sync your tasks on any device. Tasks are automatically synced on changes until you delete your user ID',
+  InstallAlert = 'You can install this PWA on your device by adding it to your home screen from the browser settings',
 }
